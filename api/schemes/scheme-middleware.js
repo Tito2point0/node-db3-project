3fab1f68582a5709662734e12576cec6a77e54a7
Tito2point0 <defconx00@gmail.com>
2024-02-01 12:51:56 -0500

    middleware done

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -35,8 +35,9 @@ const checkSchemeId = async (req, res, next) => {
 */
 const validateScheme = (req, res, next) => {
   const { scheme_name } = req.body
-  if (!scheme_name === undefined 
-    || typeof scheme_name !== 'string' || !scheme_name.trim()){
+  if (scheme_name === undefined 
+    || typeof scheme_name !== 'string'
+    || !scheme_name.trim()) {
   next({ status: 400, message: 'invalid scheme_name' })
   }
   else {
@@ -54,7 +55,20 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  const { instructions, step_number } = req.body
+ 
+  if (instructions === undefined
+    || typeof scheme_name !== 'string'
+    || !instructions.trim()
+    || typeof step_number !== 'number' 
+    || step_number < 1
+  ) {
+    const error = { status: 400, message: 'invalid step' }
+    next(error)
+  } else {
+    next()
+  }
+  
 }
 
 module.exports = {
